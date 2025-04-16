52a3575338bc36bc182e9074932df5340377332e
JANANI I <146628736+jananiilangannan8605@users.noreply.github.com>
2025-04-16 14:49:03 +0530

    Time: 0 ms (100%), Space: 45.4 MB (36%) - LeetHub

---
+++ 1001-n-repeated-element-in-size-2n-array/1001-n-repeated-element-in-size-2n-array.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int repeatedNTimes(int[] nums) {
+        Map<Integer, Integer> map=new HashMap<>();
+        for(int i:nums){
+            map.put(i,map.getOrDefault(i,0)+1);
+            if(map.get(i)>1) return i;
+        }
+        return -1;
+    }
+}
