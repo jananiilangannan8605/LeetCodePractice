83c81680ceae577758ded1bd27383788b5a00632
JANANI I <146628736+jananiilangannan8605@users.noreply.github.com>
2025-04-18 15:29:07 +0530

    Time: 0 ms (100%), Space: 41.5 MB (70.19%) - LeetHub

--- 1910-check-if-binary-string-has-at-most-one-segment-of-ones/1910-check-if-binary-string-has-at-most-one-segment-of-ones.java
+++ 1910-check-if-binary-string-has-at-most-one-segment-of-ones/1910-check-if-binary-string-has-at-most-one-segment-of-ones.java
@@ -1,8 +1,5 @@
 class Solution {
     public boolean checkOnesSegment(String s) {
-        for(int i=1;i<s.length();i++){
-            if(s.charAt(i-1)=='0' && s.charAt(i)=='1') return false;
-        }
-        return true;
+        return !s.contains("01");
     }
 }
