da01f124259c285a8d75b11fb58cf3cf21e4387d
JANANI I <146628736+jananiilangannan8605@users.noreply.github.com>
2025-04-18 15:20:27 +0530

    Time: 2 ms (93.57%), Space: 45 MB (48.55%) - LeetHub

--- 3540-hash-divided-string/3540-hash-divided-string.java
+++ 3540-hash-divided-string/3540-hash-divided-string.java
@@ -1,14 +1,14 @@
 class Solution {
     public String stringHash(String s, int k) {
-        String str="";
+        StringBuilder sb= new StringBuilder();
         for(int j=0;j<s.length();j=j+k){
         int temp=0;
         for(int i=j;i<k+j;i++){
             temp+=(s.charAt(i)-'a');
         
         }
-     str+=(char)(temp%26+'a');
+     sb.append((char)(temp%26+'a'));
         }
-     return str;
+     return sb.toString();
     }
 }
