458afb4cf4c7e4afea938dbeec4aa2662068aabb
JANANI I <146628736+jananiilangannan8605@users.noreply.github.com>
2025-06-19 16:46:39 +0530

    Time: 0 ms (100%), Space: 41.9 MB (47.72%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,8 +1,26 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-       digits[digits.length-1]= digits[digits.length-1]+1;
-       for(int i=0;i<digits.length;i++){
-        return digits;
+        boolean flag=true;
+       for(int i=digits.length-1;i>=0;i--){
+        if(digits[i]<9){
+            digits[i]++;
+            for(int j=0;j<digits.length;i++){
+                return digits;
+            }
+            flag=false;
+            break;
+        }
+        else{
+            digits[i]=0;
+        }
+       }
+       if(flag){
+        int[] res=new int[digits.length+1];
+        res[0]=1;
+        for(int i=0;i<res.length;i++){
+            return res;
+        }
+       }
+return digits;
        }
     }
-}
