b10c4737c5a4c8c19dd35391bd689e8c90fe34ed
JANANI I <146628736+jananiilangannan8605@users.noreply.github.com>
2025-06-19 16:46:25 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+       digits[digits.length-1]= digits[digits.length-1]+1;
+       for(int i=0;i<digits.length;i++){
+        return digits;
+       }
+    }
+}
