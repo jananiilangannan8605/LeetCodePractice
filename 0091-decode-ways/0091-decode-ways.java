9cf616c72c59a43559064f93b3ac5bfa4c3acf79
JANANI I <146628736+jananiilangannan8605@users.noreply.github.com>
2025-07-02 10:25:10 +0530

    Time: 1 ms (83.72%), Space: 42.4 MB (8.12%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int numDecodings(String s) {
+        int n=s.length();
+        int[]dp=new int[n+1];
+        dp[0]=1;
+        if(s.charAt(0)!='0'){
+            dp[1]=1;
+        }
+        else{
+            dp[1]=0;
+        }
+        for(int i=2;i<=n;i++){
+            if(s.charAt(i-1)!='0'){
+                dp[i]+=dp[i-1];
+            }
+            int a=Integer.parseInt(s.substring(i-2,i));
+            if(a>=10&&a<=26){
+                dp[i]+=dp[i-2];
+            }
+        }
+        return dp[n];
+    }
+}
