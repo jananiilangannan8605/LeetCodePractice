ab26612fa610ba98115d2672a539abea6af9d295
JANANI I <146628736+jananiilangannan8605@users.noreply.github.com>
2025-07-10 23:25:44 +0530

    Time: 2 ms (59.48%), Space: 44.6 MB (48.95%) - LeetHub

---
+++ 3790-fruits-into-baskets-ii/3790-fruits-into-baskets-ii.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int numOfUnplacedFruits(int[] fruits, int[] baskets) {
+        int n = fruits.length;
+        int m = baskets.length;
+        boolean[] used = new boolean[m]; // track used baskets
+        int unplaced = 0;
+
+        for (int i = 0; i < n; i++) {
+            boolean placed = false;
+            for (int j = 0; j < m; j++) {
+                if (!used[j] && fruits[i] <= baskets[j]) {
+                    used[j] = true;
+                    placed = true;
+                    break;
+                }
+            }
+            if (!placed) {
+                unplaced++;
+            }
+        }
+
+        return unplaced;
+    }
+}
