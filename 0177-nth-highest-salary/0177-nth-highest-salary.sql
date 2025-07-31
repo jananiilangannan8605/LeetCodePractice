6db59268a31feb7dc1f42a89d0c07a0f8c5ef909
JANANI I <146628736+jananiilangannan8605@users.noreply.github.com>
2025-08-01 00:44:54 +0530

    Time: 440 ms (51.71%), Space: 0B (100%) - LeetHub

--- 0177-nth-highest-salary/0177-nth-highest-salary.sql
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -1,11 +1,7 @@
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
-SET N = N-1;
+set N=N-1;
   RETURN (
-      SELECT DISTINCT(salary) from Employee order by salary DESC
-      LIMIT 1 OFFSET N
-      
+        select distinct(salary) from Employee order by salary desc limit 1 offset N
   );
 END
-
-#pls upvote if you find solution easy to undestand....!! Thanks..!!!
